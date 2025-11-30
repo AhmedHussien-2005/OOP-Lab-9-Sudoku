50451487d2044ebf73f3bba6fccda2909467c3b4
alielhadad11 <alielhadad511@gmail.com>
2025-11-30 05:16:56 +0200

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,15 @@
+package Backend;
+
+public class Pair {
+    public int firstR;
+    public int firstC;
+    public int secondR;
+    public int secondC;
+
+    public Pair(int firstR, int firstC, int secondR, int secondC){
+        this.firstR = firstR;
+        this.firstC = firstC;
+        this.secondR = secondR;
+        this.secondC = secondC;
+    }
+}
